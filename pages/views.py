8a2f83671d88bd97bd092f7d4b7d5a77b6b14e0f
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2022-07-24 20:23:33 +0200

    Add about and listing page

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,25 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from listings.models import Listing
+from realtors.models import Realtor
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.all().order_by('-list_date').filter(is_published=True)[:3]
+    return render(request, 'pages/index.html', {'listings': listings})
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    # Get MVP
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors,
+    }
+
+    return render(request, 'pages/about.html', context)
