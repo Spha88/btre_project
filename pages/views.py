147831a369a401bbef7a4f68d373a55c1f248a44
SIPHAMANDLA MEHLOMAKULU <mr.smehlomakulu@gmail.com>
2022-07-26 03:47:15 +0200

    Add search form functionality

--- pages/views.py
+++ pages/views.py
@@ -3,11 +3,20 @@ from django.shortcuts import render
 
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
-    listings = Listing.objects.all().order_by('-list_date').filter(is_published=True)[:3]
-    return render(request, 'pages/index.html', {'listings': listings})
+    listings = Listing.objects.all().order_by(
+        '-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'price_choices': price_choices,
+        'bedroom_choices': bedroom_choices,
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
